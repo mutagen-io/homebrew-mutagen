d570c53f231fc56083ab7e7d5ed064e95009e7fd
Jacob Howard <jacob@havoc.io>
2019-02-13 23:10:52 +0200

    Bumped development version to v0.8.0-beta2.
    This commit bumps the development version to v0.8.0-beta2.

--- Formula/mutagen.rb
+++ Formula/mutagen.rb
@@ -29,9 +29,9 @@ class Mutagen < Formula
   sha256 "199c75248ec2af0cba043f60a664f1953ff2cbd9398fd24b21ec7531c6d3dcf0"
 
   devel do
-    version "0.8.0-beta1"
-    url "https://github.com/havoc-io/mutagen/releases/download/v0.8.0-beta1/mutagen_darwin_amd64_v0.8.0-beta1.tar.gz"
-    sha256 "b802b3f4e357ffa456e5bc3f82891d0247f9c4b26985b1a0f2c45ee39de45ad1"
+    version "0.8.0-beta2"
+    url "https://github.com/havoc-io/mutagen/releases/download/v0.8.0-beta2/mutagen_darwin_amd64_v0.8.0-beta2.tar.gz"
+    sha256 "947fdd55c5fa6110c7219b8949e47247c725b20de860dcef294269caf3228f0d"
   end
 
   def install
