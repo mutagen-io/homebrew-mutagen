0c3badd1452487134b8e0e7134272ab673aa3e99
Jacob Howard <jacob@mutagen.io>
2021-12-07 15:16:46 -0700

    Bumped Mutagen Compose to v0.13.0-beta3-1.
    This commit bumps Mutagen Compose to v0.13.0-beta3-1.
    
    Signed-off-by: Jacob Howard <jacob@mutagen.io>

--- Formula/mutagen-compose-beta.rb
+++ Formula/mutagen-compose-beta.rb
@@ -8,18 +8,18 @@
 class MutagenComposeBeta < Formula
   desc "Mutagen integration with Docker Compose"
   homepage "https://github.com/mutagen-io/mutagen-compose"
-  version "0.13.0-beta3"
+  version "0.13.0-beta3-1"
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/mutagen-io/mutagen-compose/releases/download/v0.13.0-beta3/mutagen-compose_darwin_arm64_v0.13.0-beta3.tar.gz"
-      sha256 "0e4f8d34ddf41522a7659a876c0a1a70f4c76013f5580b44b997edf802e20ba9"
+      url "https://github.com/mutagen-io/mutagen-compose/releases/download/v0.13.0-beta3-1/mutagen-compose_darwin_arm64_v0.13.0-beta3.tar.gz"
+      sha256 "fc6177cabacad2e9f921c08bf6f62c8fb3a6962ed77fdee64e1393bcbbc49d24"
     else
-      url "https://github.com/mutagen-io/mutagen-compose/releases/download/v0.13.0-beta3/mutagen-compose_darwin_amd64_v0.13.0-beta3.tar.gz"
-      sha256 "3651a7105f7d07b6d7ebb495f2b71599b7603d8b7b6352fd9837d0811cb93d67"
+      url "https://github.com/mutagen-io/mutagen-compose/releases/download/v0.13.0-beta3-1/mutagen-compose_darwin_amd64_v0.13.0-beta3.tar.gz"
+      sha256 "0354c57426cc5c5eee177f2f1a194dcf847ea62099261aac32dc19a2ed727066"
     end
   else
-    url "https://github.com/mutagen-io/mutagen-compose/releases/download/v0.13.0-beta3/mutagen-compose_linux_amd64_v0.13.0-beta3.tar.gz"
-    sha256 "1cc3a24470355e30b6f6ed3282f116540b29c1e2cff4c272ef36d0cb6274d0d1"
+    url "https://github.com/mutagen-io/mutagen-compose/releases/download/v0.13.0-beta3-1/mutagen-compose_linux_amd64_v0.13.0-beta3.tar.gz"
+    sha256 "4908ccf1ba7d2fb4510130436b90be59e8018852a54584a9afadb54c369b6d10"
   end
 
   depends_on "mutagen-beta"
