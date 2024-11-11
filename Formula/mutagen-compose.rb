af637d3a62a132f5609f8cfc656dbf6eecb30d3d
Jacob Howard <jacob@mutagen.io>
2024-11-11 16:51:12 -0700

    Bumped Mutagen Compose formulae to v0.18.0-1.
    Signed-off-by: Jacob Howard <jacob@mutagen.io>

--- Formula/mutagen-compose.rb
+++ Formula/mutagen-compose.rb
@@ -1,18 +1,18 @@
 class MutagenCompose < Formula
   desc "Compose with Mutagen integration"
   homepage "https://github.com/mutagen-io/mutagen-compose"
-  version "0.18.0"
+  version "0.18.0-1"
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/mutagen-io/mutagen-compose/releases/download/v0.18.0/mutagen-compose_darwin_arm64_v0.18.0.tar.gz"
-      sha256 "4f98ea38cf06ee1742bf721794e1f7d6d438e7ee1b8e02448a2a827e32e9e861"
+      url "https://github.com/mutagen-io/mutagen-compose/releases/download/v0.18.0-1/mutagen-compose_darwin_arm64_v0.18.0.tar.gz"
+      sha256 "9f1889db2eb3aa3c099b985e8e2f89363653293edd37757b79607599ae11b5ee"
     else
-      url "https://github.com/mutagen-io/mutagen-compose/releases/download/v0.18.0/mutagen-compose_darwin_amd64_v0.18.0.tar.gz"
-      sha256 "987c619e2d0f56d2c9103a0659dce28faf3a331aebdadc5b20ed8d01aeda57da"
+      url "https://github.com/mutagen-io/mutagen-compose/releases/download/v0.18.0-1/mutagen-compose_darwin_amd64_v0.18.0.tar.gz"
+      sha256 "5aeb0140b299f884b80127397b2096e844b148383bdec913caa6446acbfaf45a"
     end
   else
-    url "https://github.com/mutagen-io/mutagen-compose/releases/download/v0.18.0/mutagen-compose_linux_amd64_v0.18.0.tar.gz"
-    sha256 "87d4ab81d7c5e778b427c9af09ad65dca049219944191c9f4c9d869b413d82ef"
+    url "https://github.com/mutagen-io/mutagen-compose/releases/download/v0.18.0-1/mutagen-compose_linux_amd64_v0.18.0.tar.gz"
+    sha256 "955b3e8627f7465e28505d06f1b8af07ecce4b9decb4945755c33a63af84a4ef"
   end
 
   depends_on "mutagen"
